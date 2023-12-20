e85649f0b89f148e3afd1d8003801010f5915638
Asuma Yamada <yamadaasuma@gmail.com>
2023-12-21 03:47:23 +0900

    fix

--- src/lib/trpc.tsx
+++ src/lib/trpc.tsx
@@ -1,4 +1,18 @@
-import { createTRPCReact } from "@trpc/react-query";
+import { createTRPCReact, httpBatchLink } from "@trpc/react-query";
 import type { AppRouter } from "@/server/trpc";
+import { createTRPCNext } from "@trpc/next";
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export const api = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    };
+  },
+  ssr: false,
+});
